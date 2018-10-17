a2d20545d5960ec0d03a53f5462fb8a825b37f13
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-10-17 21:15:28 +0900

    ひとまずアプリとして動作するとこまでを優先するため、com.android.library にする

--- api/src/main/java/com/github/mag0716/api/ApiClientFactory.kt
+++ api/src/main/java/com/github/mag0716/api/ApiClientFactory.kt
@@ -4,7 +4,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-internal class ApiClientFactory {
+class ApiClientFactory {
 
     fun create(): ApiService {
         return Retrofit.Builder()
--- api/src/main/java/com/github/mag0716/api/ApiService.kt
+++ api/src/main/java/com/github/mag0716/api/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-internal interface ApiService {
+interface ApiService {
     // TODO: coroutine で返す(最悪でも Rx で)
     @GET("/data")
     fun data(): Call<List<Data>>
