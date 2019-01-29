76aa0f7e7f88e69b09f5aacb53b2c3040f4abf0f
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-01-29 22:44:58 +0900

    java-library の api モジュールでも Timber を利用する

--- api/src/main/java/com/github/mag0716/api/ApiServiceModule.kt
+++ api/src/main/java/com/github/mag0716/api/ApiServiceModule.kt
@@ -5,18 +5,20 @@ import com.github.mag0716.api.response.DetailResponse
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import timber.log.Timber
+import timber.log.debug
 
 object ApiServiceModule {
 
     fun provide(): ApiService {
         return object : ApiService {
             override fun data() = GlobalScope.async {
-                println("data")
+                Timber.debug { "data" }
                 return@async createData()
             }
 
             override fun detail(id: Int) = GlobalScope.async {
-                println("detail($id)")
+                Timber.debug { "detail($id)" }
                 return@async createDetail(id)
             }
         }
