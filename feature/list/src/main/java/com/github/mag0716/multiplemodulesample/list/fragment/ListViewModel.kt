d637c417634fe83163b1751b28b2d97646110ab1
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-06-13 23:47:04 +0900

    ViewModel に処理を移行し、Fragment 側は ViewModel 側で保持する LiveData を observe し、View を更新するだけに変更

---
+++ feature/list/src/main/java/com/github/mag0716/multiplemodulesample/list/fragment/ListViewModel.kt
@@ -0,0 +1,46 @@
+package com.github.mag0716.multiplemodulesample.list.fragment
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.github.mag0716.multimodulesample.datasource.model.Data
+import com.github.mag0716.multiplemodulesample.App
+import com.github.mag0716.usercase.IDataListView
+import com.github.mag0716.usercase.IGetDataListUseCase
+import kotlinx.coroutines.launch
+
+class ListViewModel(application: Application) : AndroidViewModel(application), IDataListView {
+
+    private val getDataListUseCase: IGetDataListUseCase =
+        (application as App).provideGetDataListUseCase(this@ListViewModel)
+    private val progress = MutableLiveData<Boolean>()
+    private val dataList = MutableLiveData<List<Data>>()
+
+    fun getProgress(): LiveData<Boolean> {
+        return progress
+    }
+
+    fun getDataList(): LiveData<List<Data>> {
+        return dataList
+    }
+
+    override fun showDataList(dataList: List<Data>) {
+        this.dataList.value = dataList
+    }
+
+    override fun showLoading() {
+        progress.value = true
+    }
+
+    override fun dismissLoading() {
+        progress.value = false
+    }
+
+    fun fetchData() {
+        viewModelScope.launch {
+            getDataListUseCase.execute()
+        }
+    }
+}
