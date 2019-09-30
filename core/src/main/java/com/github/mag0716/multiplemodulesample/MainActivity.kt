0f47c81e7fe10a4d931a8de81bd68401578cfbaf
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-09-30 22:43:34 +0900

    OssLicensesMenuActivity への遷移処理追加

--- core/src/main/java/com/github/mag0716/multiplemodulesample/MainActivity.kt
+++ core/src/main/java/com/github/mag0716/multiplemodulesample/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.github.mag0716.multiplemodulesample
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.show_infomartion -> {
+                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
