2a18149d50b01961d4bdba466107361c20e462cb
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-10-03 22:31:05 +0900

    oss-license-plugin を削除

--- core/src/main/java/com/github/mag0716/multiplemodulesample/MainActivity.kt
+++ core/src/main/java/com/github/mag0716/multiplemodulesample/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.github.mag0716.multiplemodulesample
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +20,6 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.show_infomartion -> {
-                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
                 true
             }
             else -> super.onOptionsItemSelected(item)
